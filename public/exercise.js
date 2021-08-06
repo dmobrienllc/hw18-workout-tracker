4984928a96ac886a912c7b78a85a428a4e4212b8
dmobrien <dmobrienllc@gmail.com>
2021-08-06 13:56:25 -0500

    Fixed bug in add/continue workout dialog.

--- public/exercise.js
+++ public/exercise.js
@@ -1,13 +1,13 @@
 const workoutTypeSelect = document.querySelector("#type");
 const cardioForm = document.querySelector(".cardio-form");
-const resistanceForm = document.querySelector(".Resistance-form");
+const resistanceForm = document.querySelector(".resistance-form");
 const cardioNameInput = document.querySelector("#cardio-name");
 const nameInput = document.querySelector("#name");
 const weightInput = document.querySelector("#weight");
 const setsInput = document.querySelector("#sets");
 const repsInput = document.querySelector("#reps");
 const durationInput = document.querySelector("#duration");
-const resistanceDurationInput = document.querySelector("#Resistance-duration");
+const resistanceDurationInput = document.querySelector("#resistance-duration");
 const distanceInput = document.querySelector("#distance");
 const completeButton = document.querySelector("button.complete");
 const addButton = document.querySelector("button.add-another");
@@ -34,7 +34,7 @@ initExercise();
 function handleWorkoutTypeChange(event) {
   workoutType = event.target.value;
 
-  if (workoutType === "cardio") {
+  if (workoutType === "Cardio") {
     cardioForm.classList.remove("d-none");
     resistanceForm.classList.add("d-none");
   } else if (workoutType === "Resistance") {
